558342797f3cb9bbc6334220d854ad30a0b55c2e
Martijn Arts <martijn@martijnarts.com>
2025-04-13 11:19:49 +0200

    Fix color support in Tailwind

--- crates/book/src/registry/button.rs
+++ crates/book/src/registry/button.rs
@@ -1,12 +1,35 @@
 use crate::story::{register_story, StoryAsView};
-use holt_ui::visual::Button;
+use holt_ui::visual::{Button, ButtonVariant, H1};
 use leptos::prelude::*;
 
 struct ButtonStory;
 
 impl StoryAsView for ButtonStory {
     fn as_view(&self) -> AnyView {
-        view! { <Button>"Click me!"</Button> }.into_any()
+        view! {
+            <>
+                <H1>Variants</H1>
+
+                <p>Default</p>
+                <Button class="w-32">"Click me!"</Button>
+
+                <p>Destructive</p>
+                <Button class="w-32" variant=ButtonVariant::Destructive>"Click me!"</Button>
+
+                <p>Outline</p>
+                <Button class="w-32" variant=ButtonVariant::Outline>"Click me!"</Button>
+
+                <p>Secondary</p>
+                <Button class="w-32" variant=ButtonVariant::Secondary>"Click me!"</Button>
+
+                <p>Ghost</p>
+                <Button class="w-32" variant=ButtonVariant::Ghost>"Click me!"</Button>
+
+                <p>Link</p>
+                <Button class="w-32" variant=ButtonVariant::Link>"Click me!"</Button>
+            </>
+        }
+        .into_any()
     }
 }
 

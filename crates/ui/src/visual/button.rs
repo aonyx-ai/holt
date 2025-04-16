b8795a8b27bec2f1eb0bf5fe7dc9851dec2ad92c
Martijn Arts <martijn@martijnarts.com>
2025-04-16 22:40:27 +0200

    upgrade config for Tailwind 4

--- crates/ui/src/visual/button.rs
+++ crates/ui/src/visual/button.rs
@@ -6,7 +6,7 @@ use tailwind_fuse::*;
 
 #[derive(TwClass)]
 #[tw(
-    class = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+    class = "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
 )]
 struct ButtonStyle {
     variant: ButtonVariant,
